6a7d8c8e7a7847524f93d91cc7626f9a38fba13d
Paweł Czuchryta <64778637+Ybalath@users.noreply.github.com>
2023-11-28 22:20:06 +0100

    Update styles and layout in movie list component.

--- front-basic/src/app/modules/main/movie-list/movie-list.component.ts
+++ front-basic/src/app/modules/main/movie-list/movie-list.component.ts
@@ -7,82 +7,92 @@ import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-movie-list', standalone: true, imports: [CommonModule, NgbRating, FormsModule], template: `
-        <div class="movie-list-page">
 
-            <div class="movie-filter pb-2 pt-3">
-                <div class="movie-filter-filers ">
-                    <div class="movie-filter__search card p-3">
-                        <label for="movie_name" class="form-label">Szukaj filmu</label>
-                        <input type="text" class="form-control" id="movie_name" placeholder="Wpisz nazwę filmu"
-                               [(ngModel)]="searchedMovieName">
-                    </div>
-                    <div class="card p-3">
-                        <label for="moviesSort" class="form-label">Ocena</label>
-                        <select class="form-control" [(ngModel)]="sortMoviesBy" name="moviesSort">
-                            <option value="name">Sortuj po nazwie rosnąco</option>
-                            <option value="premiere">Sortuj po dacie premiery rosnąco</option>
-                            <option value="rating">Sortuj po ocenie rosnąco</option>
-                            <option value="name_m">Sortuj po nazwie malejąco</option>
-                            <option value="premiere_m">Sortuj po dacie premiery malejąco</option>
-                            <option value="rating_m">Sortuj po ocenie malejąco</option>
-                        </select>
-                    </div>
-                    <div class="card p-3">
-                        <label for="genre" class="form-label">Gatunek</label>
-                        <select [(ngModel)]="selectedGenres" class="form-select" id="genre" name="genre">
-                            <option *ngFor="let genre of genres" [value]="genre">{{genre}}</option>
-                        </select>
-                    </div>
+        <div class="movie-filter pb-2 pt-3 mb-4 shadow">
+            <div class="movie-filter-filers d-flex justify-content-evenly">
+                <div class="movie-filter__search card p-3">
+                    <label for="movie_name" class="form-label">Szukaj filmu</label>
+                    <input type="text" class="form-control" id="movie_name" placeholder="Wpisz nazwę filmu"
+                           [(ngModel)]="searchedMovieName">
                 </div>
-                <div>
-                    <button class="btn btn-primary" (click)="filterMovies()">Filtruj</button>
+                <div class="card p-3">
+                    <label for="moviesSort" class="form-label">Ocena</label>
+                    <select class="form-control" [(ngModel)]="sortMoviesBy" name="moviesSort">
+                        <option value="name">Sortuj po nazwie rosnąco</option>
+                        <option value="premiere">Sortuj po dacie premiery rosnąco</option>
+                        <option value="rating">Sortuj po ocenie rosnąco</option>
+                        <option value="name_m">Sortuj po nazwie malejąco</option>
+                        <option value="premiere_m">Sortuj po dacie premiery malejąco</option>
+                        <option value="rating_m">Sortuj po ocenie malejąco</option>
+                    </select>
+                </div>
+                <div class="card p-3">
+                    <label for="moviesSort" class="form-label">Ocena</label>
+                    <select class="form-control" [(ngModel)]="selectedRating" name="moviesSort">
+                        <option value="1">*</option>
+                        <option value="2">**</option>
+                        <option value="3">***</option>
+                        <option value="4">****</option>
+                        <option value="5">*****</option>
+                    </select>
+                </div>
+                <div class="card p-3">
+                    <label for="genre" class="form-label">Gatunek</label>
+                    <select [(ngModel)]="selectedGenres" class="form-select" id="genre" name="genre">
+                        <option *ngFor="let genre of genres" [value]="genre">{{genre}}</option>
+                    </select>
                 </div>
             </div>
-            <div class="movie-list">
-                <ng-container *ngFor="let movie of movieList" class="movie-title-list">
-                    <div class="movie flex-md-row mb-2 rounded p-2 shadow " (click)="goToDetails(movie.id!)">
-                        <div class="movie__rating">
-                            <h5 class="title">
-                                Oceny
-                            </h5>
-                            <div class="rating__count pb-3">
-                                Liczba Ocen {{movie.numberOfRatings}}
-                            </div>
-
-                            <div class="rating__stars">
-                                Ocena <br> <ngb-rating [(rate)]="movie.rating" [max]="5" [readonly]="true"></ngb-rating>
-                            </div>
+            <div>
+                <button class="btn btn-primary" (click)="filterMovies()">Filtruj</button>
+            </div>
+        </div>
+        <div class="movie-list">
+            <ng-container *ngFor="let movie of movieList" class="movie-title-list">
+                <div class="movie row mb-3 p-3 rounded  shadow justify-content-between"
+                     (click)="goToDetails(movie.id!)">
+                    <div class="movie__rating col-1">
+                        <h5 class="title">
+                            Oceny
+                        </h5>
+                        <div class="rating__count pb-3">
+                            Liczba Ocen <br>{{movie.numberOfRatings}}
                         </div>
-                        <div class="movie__info">
-                            <h3 class="movie__title title">
-                                {{movie.name}}
-                            </h3>
 
-                            <div class="movie__plot pb-3">
-                                {{movie.description}}
-                            </div>
-                            <div class="movie__genre">
-                                Gatunek: {{movie.genre}}
-                            </div>
-                            <div class="movie__year">
-                                Premiera: {{movie.premiere}}
-                            </div>
-                        </div>
-                        <div class="movie__poster ">
-                            <img [src]="movie.gallery.url" alt="movie poster">
+                        <div class="rating__stars">
+                            Ocena <br>
+                            <ngb-rating [(rate)]="movie.rating" [max]="5" [readonly]="true"></ngb-rating>
                         </div>
                     </div>
-                </ng-container>
-            </div>
+                    <div class="movie__info col-8">
+                        <h3 class="movie__title title">
+                            {{movie.name}}
+                        </h3>
+
+                        <div class="movie__plot pb-3">
+                            {{movie.description}}
+                        </div>
+                        <div class="movie__genre">
+                            Gatunek: {{movie.genre}}
+                        </div>
+                        <div class="movie__year">
+                            Premiera: {{movie.premiere}}
+                        </div>
+                    </div>
+                    <div class="movie__poster col">
+                        <img [src]="movie.gallery.url" class="img-fluid" alt="movie poster">
+                    </div>
+                </div>
+            </ng-container>
         </div>
   `, styleUrl: './movie-list.component.scss'
 })
 export class MovieListComponent {
   mms = inject(MovieMockService)
-  genres: string[] = [`Dokument`, `Animacja`, `Fantasy`, `Horror`, `Akcja`]
+  genres: string[] = ['Wszystkie',`Dokument`, `Animacja`, `Fantasy`, `Horror`, `Akcja`]
   selectedGenres: string[] = [`Dokument`, `Animacja`, `Fantasy`, `Horror`, `Akcja`]
   searchedMovieName?: string = undefined
-  searchedRating: number = 0
+  selectedRating: number = 0
   sortMoviesBy: string = 'name';
   movieList: Movie[] = []
 
@@ -99,8 +109,8 @@ export class MovieListComponent {
           return true
         }
       })
-      .filter(movie => this.selectedGenres.includes(movie.genre))
-      .filter(movie => movie.rating >= this.searchedRating)
+      .filter(movie => this.selectedGenres.includes('Wszystkie')? true:this.selectedGenres.includes(movie.genre))
+      .filter(movie => movie.rating >= this.selectedRating)
       .sort((a, b) => {
         let multiplier = this.sortMoviesBy.endsWith('_m') ? -1 : 1;
         let property = this.sortMoviesBy.replace('_m', '');
