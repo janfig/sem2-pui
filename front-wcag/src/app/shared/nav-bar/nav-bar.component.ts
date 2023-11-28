db28dd0d08fd9a82c75fadac85e4e068fb85b27b
Krzysztof Caban <kcaban99@gmail.com>
2023-11-28 18:43:09 +0100

    added wcag-controlls to navbar

--- front-wcag/src/app/shared/nav-bar/nav-bar.component.ts
+++ front-wcag/src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterLink} from "@angular/router";
+import {NgbDropdown, NgbDropdownMenu, NgbDropdownToggle} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, NgbDropdown, NgbDropdownMenu, NgbDropdownToggle],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
